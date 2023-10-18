3668100961f5e9c8c63f3506ae7f2a75e0e75b7f
solomiyasabat <solomiyasabat@gmail.com>
2023-10-18 16:59:50 +0300

    add Angular Material lib, improve app design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule, MatCardModule],
   providers: [DatePipe],
   bootstrap: [AppComponent],
 })
